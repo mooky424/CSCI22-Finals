965d99321c97c5790791dcd009a145556a9917ef
mooky424 <66621703+mooky424@users.noreply.github.com>
2023-04-25 14:18:04 +0800

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,2 +1,21 @@
 public class Dice {
+  
+  private int count;
+  private double x;
+  private double y;
+  private double angle;
+  
+  public Dice(double x, double y, double angle, int count){
+    this.x = x;
+    this.y = y;
+    this.angle = angle;
+    this.count = count
+  }
+  
+  public rollDice(){
+    //randomizer 1-6
+  }
+  
+  public adjustX(){
+  }
 }
