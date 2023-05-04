89d9626590114cff0f953776e5fde4c296e986fc
mooky424 <gabosalvador2004@gmail.com>
2023-05-05 02:16:36 +0800

    server test

--- GameServer.java
+++ GameServer.java
@@ -4,20 +4,67 @@ import java.util.*;
 
  public class GameServer {
 
+    private ArrayList<ClientThread> clients = new ArrayList<ClientThread>();
+    private int currentUsers;
+
     public GameServer(){
+        currentUsers = 0;
         try {
-            ServerSocket ss = new ServerSocket(64820)
-            Socket s = ss.accept();
-            DataInputStream in = new DataInputStream(s.getInputStream());
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            
-            String msg = in.readUTF();
-            System.out.println(msg);
-            out.writeUTF("Pong");
-            out.flush();
+            ServerSocket ss = new ServerSocket(64820);
+            while (true){
+                Socket s = ss.accept();
+                clients.add(new ClientThread(s, currentUsers));
+                clients.get(currentUsers++).run();
+            }
             
         } catch (IOException ex){
-            System.out.println("Error: " + ex);
+            System.out.println("Error at GameServer: " + ex);
+        }
+    }
+
+    private class ClientThread implements Runnable{
+
+        Socket s;
+        int id;
+        DataInputStream in;
+        DataOutputStream out;
+
+        private ClientThread(Socket s, int id) {
+            this.s = s;
+            this.id = id;
+            try {
+                in = new DataInputStream(s.getInputStream());
+                out = new DataOutputStream(s.getOutputStream());
+            } catch (Exception ex) {
+                System.out.println("Error at ClientThread constructor: " + ex);
+            }
+        }
+
+        public void run(){
+            try {
+                while(true) {
+                    String msg = in.readUTF();
+                    sendToAll(msg);
+                    System.out.println(id + " says " + msg);
+                }
+            } catch (IOException ex) {
+                System.out.println("Error at ClientThread: " + ex);
+            }
+
+        }
+        public void write(String msg){
+            try {
+                out.writeUTF(msg);
+                out.flush();
+            } catch (IOException ex) {
+                System.out.println("Error at write: " + ex);
+            }
+        }
+    }
+
+    private void sendToAll(String msg){
+        for (ClientThread ct : clients){
+            ct.write(msg);
         }
     }
             
--- Player.java
+++ Player.java
@@ -4,17 +4,42 @@ import java.util.*;
 
 public class Player {
 
+    private Socket s;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Scanner userInput;
+
     public Player() {
+        userInput = new Scanner(System.in);
         try {
-            Socket s = new Socket("localhost", 64820);
-            DataInputStream in = new DataInputStream(s.getInputStream());
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            out.writeUTF("Ping");
-            out.flush();
-            String msg = in.readUTF();
-            System.out.println(msg);
+            s = new Socket("localhost", 64820);
+            in = new DataInputStream(s.getInputStream());
+            out = new DataOutputStream(s.getOutputStream());
+            receiveMessages rm = new receiveMessages();
+            rm.run();
+            while(true) {
+                out.writeUTF(userInput.nextLine());
+                out.flush();
+            }
         } catch (IOException ex) {
             System.out.println("Error: " + ex);
         }
+    }
+
+    private class receiveMessages implements Runnable {
+        public void run(){
+            try {
+                while (true){
+                    String msg = in.readUTF();
+                    System.out.println(msg);
+                }
+            } catch (IOException ex) {
+                System.out.println("Error at receiveMessages: " + ex);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Player p = new Player();
     }
 }
