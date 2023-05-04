17196e3707b24cc8499af264daf7d1cea20fc656
mooky424 <gabosalvador2004@gmail.com>
2023-05-05 00:48:38 +0800

    update

--- GameServer.java
+++ GameServer.java
@@ -3,21 +3,24 @@ import java.net.*;
 import java.util.*;
 
  public class GameServer {
-    try {
-        ServerSocket ss = new ServerSocket(64820)
-        Socket s = ss.accept();
-        DataInputStream in = new DataInputStream(s.getInputStream());
-        DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-        String msg = in.readUTF();
-        System.out.println(msg);
-        out.writeUTF("Pong");
-        out.flush();
-
-    } catch (IOException ex){
-        System.out.println("Error: " + ex);
+    public GameServer(){
+        try {
+            ServerSocket ss = new ServerSocket(64820)
+            Socket s = ss.accept();
+            DataInputStream in = new DataInputStream(s.getInputStream());
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            
+            String msg = in.readUTF();
+            System.out.println(msg);
+            out.writeUTF("Pong");
+            out.flush();
+            
+        } catch (IOException ex){
+            System.out.println("Error: " + ex);
+        }
     }
-
+            
     public static void main(String[] args){
         GameServer gm = new GameServer();
     }
