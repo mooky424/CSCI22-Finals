6ec72bc99887cc41482d17a16b77eef32ea26f74
Zo Esmeris <janel.esmeris@obf.ateneo.edu>
2023-05-16 00:47:27 +0800

    nabingi ak

--- GameStarter.java
+++ GameStarter.java
@@ -1,6 +1,6 @@
 /**
-	This class starts the game from the player's side. hahahah isa
-    pang sentence lods
+	This class starts the game from the player's side.
+	It also loops a sound file.
 	
 	@author Gabriel L. Salvador (225593)
     @author Janel Zherry A. Esmeris (222455)
@@ -21,13 +21,29 @@
 	of my program.
 */
 
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
+
 public class GameStarter {
-    public static void main (String[] args){
-        GameFrame lf1 = new GameFrame(1024, 768);
-        lf1.setupMainMenu();
-        GameFrame lf2 = new GameFrame(1024, 768);
-        lf2.setupMainMenu();
-        GameServer gm = new GameServer();
+    public static void main (String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		// Rollbar. (2021, March 24). How to use the Throws keyword in Java (and when to use Throw). https://rollbar.com/blog/how-to-use-the-throws-keyword-in-java-and-when-to-use-throw
+        GameFrame lf = new GameFrame(1024, 768);
+        lf.setupMainMenu();
+        // GameFrame lf2 = new GameFrame(1024, 768);
+        // lf2.setupMainMenu();
+        // GameServer gm = new GameServer();
+
+		// Bro Code. (2020, October 19). Java audio 🔊 [Video file]. YouTube. https://youtu.be/SyZQVJiARTQ?list=RDCMUC4SVo0Ue36XCfOyb5Lh1viQ
+		File file = new File("./assets/background/AquaBarbieGirlInstrumental.wav");
+		AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+		Clip clip = AudioSystem.getClip();
+		clip.open(audioStream);
+
+		while(true) {
+			clip.start();
+			clip.loop(Clip.LOOP_CONTINUOUSLY); // Firnaz. (2013, October 30). Audio Clip won't loop continuously. Stack Overflow. https://stackoverflow.com/a/19678865
+		}
     }
 
 }
