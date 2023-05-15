dddf5e1cf02c55cfa1bbc3ee97f33330b5af5a0b
Zo Esmeris <janel.esmeris@obf.ateneo.edu>
2023-05-15 23:52:02 +0800

    UMMMMMM IDEK mema descs and andito n manual pdf

--- GameGUI.java
+++ GameGUI.java
@@ -3,8 +3,22 @@
 	
 	@author Gabriel L. Salvador (225593)
     @author Janel Zherry A. Esmeris (222455)
-	@version May ?, 2023
+	@version May 15, 2023
 **/
+
+/*
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+*/
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionListener;
--- Player.java
+++ Player.java
@@ -2,8 +2,23 @@
 	class desc w a min of two sentences
 	
 	@author Gabriel L. Salvador (225593)
-	@version May ?, 2023
+    @author Janel Zherry A. Esmeris (222455)
+	@version May 15, 2023
 **/
+
+/*
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+*/
 
 import javax.swing.*;
 import java.awt.*;
